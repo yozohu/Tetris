ef5b300357668b2eb61d452f2093bb061d1b331d
Yusra Ozohu <yozohu1@gmail.com>
2019-02-13 12:57:06 -0500

    added comments

--- TetrisBoard.java
+++ TetrisBoard.java
@@ -1,12 +1,40 @@
 public class TetrisBoard{
-	Block[][] board;
+	
+	/**
+	* 2-D array of the blocks which make up the tetris board
+	*/
+	Block[][] board; 
+
+	/**
+	* 2-D array which holds the x & y coordinates of the 4 falling blocks
+	*/
 	int[][] fallingPiecePos;
+
+	/**
+	* flag for whether a piece is currently falling or not
+	*/	
 	boolean pieceFalling;
+
+	/**
+	* enum class for the Different types of Tetrimono pieces
+	*/	
 	enum Tetrimono{O,L,S,Z,J,T}
-	int numOfRows, numOfCols;
+
+	/**
+	* number of rows on the board
+	*/
+	int numOfRows;
+
+	/**
+	* number of columns on the board
+	*/
+	int numOfCols;
 
 
-
+	/**
+	* Constructor to initialize an empty board variables
+	*
+	*/
 	public TetrisBoard(){
 		numOfCols = 20;
 		numOfRows = 20;
@@ -21,11 +49,23 @@ public class TetrisBoard{
 		pieceFalling = false;
 	}
 
+
+	/**
+	* Drops a new tetrimono piece to the top - middle of the board.
+	* Sets the fallingPiecePos array to respective values.
+	* @param type the type of Tetrimono to drop
+	*
+	*/
 	public void dropNewPiece(Tetrimono type){
-		int mid = numOfCols/2;
+		int mid = numOfCols/2; //index of the center column of board
+		Block.Color c; //Color of the new piece
+
 		switch(type){
-			/*
+			
 			case O: 
+
+
+			/*
 
 			case L:
 
@@ -49,16 +89,23 @@ public class TetrisBoard{
 
 			fallingPiecePos[3][0] = 1;
 			fallingPiecePos[3][1] = mid;
+
+			c = Block.Color.YELLOW;
+			break;
 			
 		}
 
 		for(int i = 0 ; i < fallingPiecePos.length; i++){
-			board[fallingPiecePos[i][0]][fallingPiecePos[i][1]].setColor(Block.Color.YELLOW);
+			board[fallingPiecePos[i][0]][fallingPiecePos[i][1]].setColor(c);
 		}
 
 		pieceFalling = true;
 	}
 
+
+	/**
+	* Prints string representation of the board.
+	*/
 	public void displayBoard(){
 		String res = "";
 		for(int i =0; i< board.length; i++){
@@ -76,9 +123,47 @@ public class TetrisBoard{
 		System.out.println(res);
 	}
 
+
+	/**
+	* Rotates the falling piece clockwise.
+	*
+	*/
+	public void rotateFallingPiece(){
+
+	}
+
+	/**
+	* Rotates the falling piece 1 unit left if possible. 
+	*
+	*/
+	public void shiftPieceLeft(){
+
+	}
+
+	/**
+	* Shifts the falling piece 1 unit right if possible.
+	*
+	*/
+	public void shiftPieceRight(){
+		
+	}
+
+	/**
+	* Clears the row specified by int row, then moves down all the blocks above.
+	*
+	*/
+	public void clearRow(int row){
+
+	}
+
+	/**
+	* main method; currently used for testing
+	*/
 	public static void main(String[] args){
 		TetrisBoard t = new TetrisBoard();
 		t.dropNewPiece(Tetrimono.T);
 		t.displayBoard();
 	}
+
+
 }
